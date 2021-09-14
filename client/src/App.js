6fd23a5e685168c45508e88262021d116d0350d3
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-14 13:49:49 -0400

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/ProProtectedRoutetectedRoute';
 
 import Login from './components/Login';
 import Logout from './components/Logout';
