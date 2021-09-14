631b22ca60e161b63894afa7cbb3f91a0ef9af87
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-14 13:54:25 -0400

    lecture work-in-progress

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 const ProtectedRoute = ({component:Component, ...rest})=> {
-    return <Route {...rest} render={()=> {
+    return <Route {...rest} render={(props)=> {
+        console.log(props);
         return <Component/>
     }}/>
 }
