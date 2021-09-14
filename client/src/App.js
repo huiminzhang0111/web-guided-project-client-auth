d5c517117d60600d15a44a606c02033a94ffd75e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-14 13:44:42 -0400

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import Logout from './components/Logout';
 import GasPrices from './components/GasPrices';
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   const isLoggedIn = localStorage.getItem("token");
 
   return (
